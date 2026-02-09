e1a36c12a8f9abaa604626ebb5e1fc46cb33c6b0
NaimurRahmannn <niamurreiman@gmail.com>
2026-02-09 15:12:47 +0600

    fix models

--- Reciepe_Project/core/settings.py
+++ Reciepe_Project/core/settings.py
@@ -70,26 +70,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Reciepe_Project',
+        'USER': 'postgres',
+        'PASSWORD': 'jani na',
+        'HOST': 'localhost',
     }
 }
-
-# PostgreSQL configuration (uncomment below and comment above to use PostgreSQL)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'Reciepe_Project',
-#         'USER': 'postgres',
-#         'PASSWORD':'jani na',
-#         'HOST': 'localhost',
-#     }
-# }
 
 
 # Password validation
--- Reciepe_Project/reciepe_user/models.py
+++ Reciepe_Project/reciepe_user/models.py
@@ -1,11 +1,24 @@
-from django.db import models
+﻿from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Recipe(models.Model):
-       user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
-       recipe_name=models.CharField(max_length=100)
-       ingredient=models.CharField()
-       description=models.CharField()
-       recipe_image=models.ImageField(upload_to='recipe_images/', blank=True, null=True)
-       def __str__(self):
-           return self.recipe_name
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    recipe_name = models.CharField(max_length=100)
+    ingredient = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    recipe_image = models.ImageField(upload_to='recipe_images/', blank=True, null=True)
+    def __str__(self):
+        return self.recipe_name
+class StudentId(models.Model):
+    student_id = models.CharField(max_length=100)
+    def __str__(self):
+        return self.student_id
+class Department(models.Model):
+    department_name = models.CharField(max_length=100)
+    student_id = models.OneToOneField(StudentId, related_name="student", on_delete=models.CASCADE)
+    student_name = models.CharField(max_length=100)
+    student_email = models.CharField(max_length=100, unique=True)
+    student_age = models.IntegerField(default=18)
+    student_address = models.TextField()
+    def __str__(self):
+        return self.student_name
